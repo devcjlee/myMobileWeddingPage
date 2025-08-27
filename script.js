e449201c326082e671cf51d920b81d9c5b8eccb5
Chijong Lee <chijong.lee918@gmail.com>
2025-08-27 13:34:39 +0900

    commit

--- script.js
+++ script.js
@@ -1,6 +1,62 @@
+document.addEventListener ("DOMContentLoaded", () => {
+  // 1. 변수 선언
+  const welcomeMessage = "저희 두 사람의 소중한 날에 함께해 주세요.";
+  const groomFullName = "고길동";
+  const brideFullName = "둘리맘";
+  const groomFirstName = groomFullName.slice(1);
+  const brideFirstName = brideFullName.slice(1);
+  const groomFatherFullName = "고아빠";
+  const groomMotherFullName = "고엄마";
+  const brideFatherFullName = "둘아빠";
+  const brideMotherFullName = "둘엄마";
+  const weddingYear = "2026";
+  const weddingMonth = "04";
+  const weddingDay = "26";
+  const weddingDayOfWeek = "일";
+  const weddingHour = "오전 11";
+  const weddingLocation = "밀리토피아 바이 마린 웨딩센터 2층 아이리스홀";
+  const weddingLocationAddress = "경기도 성남시 강남구 1532-12";
+  const weddingLocationContact = "031-0000-0000";
+
+  // 2. title 업데이트
+  document.title = `${groomFirstName} ❤️ ${brideFirstName}의 모바일 청첩장`;
+
+  const dataMap = {
+    welcomeMessage,
+    groomFullName,
+    brideFullName,
+    groomFirstName,
+    brideFirstName,
+    groomFatherFullName,
+    groomMotherFullName,
+    brideFatherFullName,
+    brideMotherFullName,
+    weddingYear,
+    weddingMonth,
+    weddingDay,
+    weddingDayOfWeek,
+    weddingHour,
+    weddingLocation,
+    weddingLocationAddress,
+    weddingLocationContact
+  };
+
+  document.querySelectorAll("[data-name]").forEach(el => {
+    const key = el.dataset.name;
+    if (dataMap[key]) {
+      el.textContent = dataMap[key];
+    }
+  });
+
+  updateDday();
+});
+
+
+// 지도 열기
 function openMap() {
-  window.open("https://map.kakao.com/link/map/웨딩홀,37.123456,127.123456", "_blank");
+  window.open("https://map.kakao.com/link/search/밀리토피아 바이 마린 웨딩센터", "_blank");
 }
+
 // 디데이 계산
 function updateDday() {
   const weddingDate = new Date("2026-04-26T11:00:00+09:00"); // 결혼식 날짜
@@ -13,6 +69,3 @@ function updateDday() {
   const ddayText = diffDays > 0 ? `D-${diffDays}` : (diffDays === 0 ? "오늘!" : `D+${Math.abs(diffDays)}`);
   document.getElementById("dday").textContent = `(${ddayText})`;
 }
-
-// 페이지 로드 시 디데이 업데이트
-updateDday();
